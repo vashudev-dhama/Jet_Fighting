d2fd40afa505cb20e8c592de335791ff0340d1b7
Vashudev Dhama <dhamavashu1119@gmail.com>
2019-09-05 02:48:41 +0530

    Splash screen with audio added and terrain modified a bit.

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstScreen", 2f);
+
+    }
+
+    void LoadFirstScreen()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
