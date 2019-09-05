44a5e7710a2a4e4ad6ff6b6d0130f1ce6c6b802b
Vashudev Dhama <dhamavashu1119@gmail.com>
2019-09-05 20:06:30 +0530

    Waypoint tracker utility script added

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MusicPlayer : MonoBehaviour
 {
+    //Awake() executes before Start() 
+    //To keep music playing after loading scene 1 from splash screen.
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject); //Built-in function to keep the gameObject from destroying.
+    }
     // Start is called before the first frame update
     void Start()
     {
+        //Load level 1 after 2 sec from splash screen.
         Invoke("LoadFirstScreen", 2f);
-
     }
 
+    //To load scene 1
     void LoadFirstScreen()
     {
         SceneManager.LoadScene(1);
