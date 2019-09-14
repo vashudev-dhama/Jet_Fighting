2c0643e01705642fa9e2ef7082f83eea5df126a2
vashudev-dhama <dhamavashu1119@gmail.com>
2019-09-14 21:57:35 +0530

    Horizontal cross input and sensitivity

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        //print(horizontalThrow);
+
+        float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
+    }
+}
