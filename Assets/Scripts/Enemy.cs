6392da6669c61dc11beb16680465a81506815812
vashudev-dhama <dhamavashu1119@gmail.com>
2019-09-16 02:22:22 +0530

    Simple score board UI added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,14 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 12;
+
+    ScoreBoard scoreBoard;
+
     // Start is called before the first frame update
     void Start()
     {
         //any gameobject with this script will generate a box collider with it
-        AddNonTriggerBoxCollider();   
+        AddNonTriggerBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>(); //find reference to the score board
     }
 
     private void AddNonTriggerBoxCollider()
@@ -21,7 +27,11 @@ public class Enemy : MonoBehaviour
     //destroy game object (enemy) when hitted by laser bullets
     void OnParticleCollision(GameObject other)
     {
+        //Instantiate(prefabToUse, worldSpacePosition, Quanternion.identity) for runtime instantiation of enemy's explosion
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent; //set fx's local position parent
         //print("Particles collided with enemy" + gameObject.name);
         Destroy(gameObject);
+        scoreBoard.ScoreHit(scorePerHit);
     }
 }
---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreBoard : MonoBehaviour
+{
+    int score = 0;
+    Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();//get the Text component
+        scoreText.text = score.ToString(); //update the text of the component
+    }
+
+    public void ScoreHit(int scoreIncrease)
+    {
+        score += scoreIncrease;
+        scoreText.text = score.ToString(); //update text component
+
+    }
+}
