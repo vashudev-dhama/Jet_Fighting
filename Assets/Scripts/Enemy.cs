d3c3919eb92c2540597e354e3ff1f833e98b01a5
vashudev-dhama <dhamavashu1119@gmail.com>
2019-09-17 01:31:46 +0530

    Enemy health hits and level design modifications

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,7 +7,7 @@ public class Enemy : MonoBehaviour
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 12;
-
+    [SerializeField] int hits = 1;
     ScoreBoard scoreBoard;
 
     // Start is called before the first frame update
@@ -27,11 +27,20 @@ public class Enemy : MonoBehaviour
     //destroy game object (enemy) when hitted by laser bullets
     void OnParticleCollision(GameObject other)
     {
+        hits--;
+        if(hits <= 0)
+        {
+            KillEnemy();
+        }
+        
+        scoreBoard.ScoreHit(scorePerHit);
+    }
+
+    private void KillEnemy()
+    {
         //Instantiate(prefabToUse, worldSpacePosition, Quanternion.identity) for runtime instantiation of enemy's explosion
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent; //set fx's local position parent
-        //print("Particles collided with enemy" + gameObject.name);
         Destroy(gameObject);
-        scoreBoard.ScoreHit(scorePerHit);
     }
 }
